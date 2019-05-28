1bb22b8cce45849768c2aff456699a921556c4c1
suyog <suyog@abiyan.com>
2019-05-28 16:46:17 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { OrderComponent } from './order/order.component';
+import { DetailsComponent } from './details/details.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { AddnewCustomersComponent } from './addnew-customers/addnew-customers.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerListComponent,
+    CustomerDetailsComponent,
+    OrderComponent,
+    DetailsComponent,
+    OrderDetailsComponent,
+    AddnewCustomersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
